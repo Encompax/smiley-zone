3f3bed8cd92462dc2e8eb9c151ef81e82db3a7d3
Brian <brichardson7170@gmail.com>
2025-04-14 07:36:59 -0400

    added images path

--- scripts/games/fact.js
+++ scripts/games/fact.js
@@ -1,11 +1,15 @@
 
 const facts = [
-  { text: "Winston Churchill was once hit by a car in New York and apologized to the driver.", image: "churchill-hit-by-car.png" },
-  { text: "Ancient Egyptians used slabs of stone as pillows.", image: "egyptian-stone-pillows.png" },
-  { text: "Napoleon was once attacked by a horde of bunnies during a hunting trip.", image: "napoleon-vs-bunnies.png" },
-  { text: "The Terracotta Army was discovered by farmers digging a well in 1974.", image: "terracotta-army-discovery.png" },
-  { text: "During the Victorian era, people used to take photos with their deceased relatives.", image: "victorian-post-mortem.png" },
-  { text: "The Eiffel Tower can grow up to 6 inches taller in summer due to metal expansion.", image: "eiffel-tower-summer.png" }
+  { text: "Winston Churchill was once hit by a car in New York and apologized to the driver.", image: "images/winswreck.png" },
+  { text: "Ancient Athens made it illegal to cut down olive trees—even on your own land.", image: "images/cutdownolive.png" },
+  { text: "Napoleon was once attacked by a horde of bunnies during a hunting trip.", image: "images/naprabbit.png" },
+  { text: "A pope once resigned voluntarily in the 13th century and was imprisoned by his successor.", image: "images/poperesign.png" },
+  { text: "The shortest war in history, between Britain and Zanzibar, lasted just 38 minutes.", image: "images/shortwar.png" },
+{ text: "Cleopatra lived closer in time to the iPhone than to the building of the Great Pyramid.", image: "images/cleophone.png" },
+{ text: "Isaac Newton stuck a needle in his eye socket to study how light and vision worked.", image: "images/isaacneedle.png" },
+{ text: "After his death in 1955, Albert Einstein’s brain was removed—without his family’s permission—by the pathologist Thomas Harvey, who kept it in jars and later distributed pieces to researchers.", image: "images/einbrain.png" },
+{ text: "King Gustav III of Sweden was shot at a masquerade ball in 1792—an event that later inspired operas and films.", image: "images/gustav.png" },
+  { text: "The Eiffel Tower can grow up to 6 inches taller in summer due to metal expansion.", image: "images/eiffelsix.png" }
 ];
 
 function generateFact() {
