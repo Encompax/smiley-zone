95dd9ebdd31c659129926bb3ff766cfc5da7c053
Brian <brichardson7170@gmail.com>
2025-04-15 19:36:11 -0400

    chnaged if (page !=="home") to "index"

--- scripts/router.js
+++ scripts/router.js
@@ -19,7 +19,7 @@ function loadGame() {
             }
 
             // Load game-specific JavaScript if not index
-            if (page !== "home") {
+            if (page !== "index") {
                 const script = document.createElement("script");
                 script.src = `scripts/games/${page}.js`;
                 script.id = "dynamicScript";
